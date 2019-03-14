2cb4f6bffeaedd3d9c25aab99dc8493775013280
Caio Braga Silva <42775945+caiobsilva@users.noreply.github.com>
2019-03-14 20:41:05 -0300

    server

---
+++ CartagenaServidores/CartagenaServidores/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CartagenaServer;
+
+namespace CartagenaServidores
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            string lista = Jogo.ListarPartidas("T");
+            string[] linha;
+            linha = lista.Split('\n');
+
+            for (int i = 0; i < linha.Length; i++)
+            {
+                linha[i].Replace("\r","");
+            }
+
+            for (int i = 0; i < linha.Length; i++)
+            {
+                lsbPartidas.Items.Add(linha[i]);
+            }
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCriar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CartagenaServidores/CartagenaServidores/Partida.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartagenaServidores
+{
+    class Partida
+    {
+        string nome { get; set; }
+        string senha { get; set; }
+
+         
+    }
+}

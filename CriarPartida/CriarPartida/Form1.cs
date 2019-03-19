8237d7c2369121afdc1009ec23f497c096842203
MathEiji <43216651+MathEiji@users.noreply.github.com>
2019-03-19 00:30:22 -0300

    Funcao de criar partidas - FEITA

---
+++ CriarPartida/CriarPartida/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CartagenaServer;
+
+namespace CriarPartida
+{
+    public partial class Form1 : Form
+    {
+        int salaID;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalaCriar_Click(object sender, EventArgs e)
+        {
+            salaID = Convert.ToInt32(Jogo.CriarPartida(txtSalaNome.Text, txtSalaSenha.Text));
+            MessageBox.Show(salaID.ToString());
+        }
+    }
+}
